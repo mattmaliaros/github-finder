b60cafc5b8fa37d4b99f40042a815503021f4374
Matt <mattmaliaros@gmail.com>
2019-09-13 16:45:09 -0400

    First

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment, Component } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    // const numbers = [1, 2, 3, 4];
+    // const loading = false;
+    // const showName = true;
+
+    return (
+      <div className='app'>
+        <Navbar />
+        <div className='container'>
+          <Users />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
