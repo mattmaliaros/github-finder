6020131016828e6cb80acc08c41a27ff0570346a
Matt <mattmaliaros@gmail.com>
2019-09-16 16:39:59 -0400

    Monday changes pt2

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null
   };
@@ -44,6 +45,15 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   };
 
+  //Get Users Repos
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ repos: res.data, loading: false });
+  };
+
   //Clears Github Users
   clearUsers = () => {
     this.setState({ users: [], loading: false });
@@ -56,7 +66,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, loading, user } = this.state;
+    const { users, loading, repos, user } = this.state;
     // const numbers = [1, 2, 3, 4];
     // const loading = false;
     // const showName = true;
@@ -92,7 +102,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
